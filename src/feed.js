96581231f697740b98db99cf2bf8d9b9078fc0d7
NINETENTWO <91178761+tradingbotswapmeet@users.noreply.github.com>
2026-02-09 00:42:55 -0800

    fix(build): correct CodeEditor import path

--- src/feed.js
+++ src/feed.js
@@ -5,7 +5,7 @@
 
 import { auth, posts, formatRelativeTime } from './services/auth.js';
 import { GithubBrowser } from './components/GithubBrowser.js';
-import { CodeEditor } from './components/CodeEditor.js';
+import { CodeEditor } from './components/code-editor.js';
 import { ActivityPicker } from './components/ActivityPicker.js';
 import { MediaPicker } from './components/MediaPicker.js';
 import { CommentThread } from './components/CommentThread.js';
